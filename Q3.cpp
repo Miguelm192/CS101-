d97bce571774cc9be64c310d68225e19efe5c6b7
Miguelm192 <113277691+Miguelm192@users.noreply.github.com>
2024-11-20 02:18:46 -0500

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -2,32 +2,38 @@
 #include <array>
 using namespace std;
 
-int main(int argc, char** argv) {
+int main() {
+    // Declare the 4x5 matrix, 5-element vector, and 4-element result vector
     array<array<double, 5>, 4> mymatrix = {};
-    array<double, 5>  myvector = {};
+    array<double, 5> myvector = {};
     array<double, 4> resultvector = {};
-    cout << "Enter a 4x5 matrix of doubles: ";
 
-    for(size_t row = 0; row < mymatrix.size(); row++)
-    {
-        for(size_t col = 0; col < mymatrix[row].size(); col++)
-        {
+    // Input the 4x5 matrix
+    cout << "Enter a 4x5 matrix of doubles:" << endl;
+    for (size_t row = 0; row < mymatrix.size(); row++) {
+        for (size_t col = 0; col < mymatrix[row].size(); col++) {
             cin >> mymatrix[row][col];
         }
     }
-    cout << "Emter a 5 element vecter of doubles: ";
-    for(size_t i = 0; 1 < myvector.size(); i++)
-        cin >> myvector[1];
 
-        for(size_t row = 0; row < mymatrix.size(); row++)
-    {
-        for(size_t col = 0; col < mymatrix[row].size(); col++)
-        {
+    // Input the 5-element vector
+    cout << "Enter a 5-element vector of doubles:" << endl;
+    for (size_t i = 0; i < myvector.size(); i++) {
+        cin >> myvector[i];
+    }
+
+    // Perform matrix-vector multiplication
+    for (size_t row = 0; row < mymatrix.size(); row++) {
+        for (size_t col = 0; col < mymatrix[row].size(); col++) {
             resultvector[row] += mymatrix[row][col] * myvector[col];
         }
     }
 
-    for(size_t i = 0; i < resultvector.size(); i++)
+    // Output the result vector
+    cout << "Result vector:" << endl;
+    for (size_t i = 0; i < resultvector.size(); i++) {
         cout << resultvector[i] << endl;
+    }
+
     return 0;
 }
