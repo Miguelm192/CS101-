b3dfe557683acb164d6245116c561fd7e88e1a3c
Miguelm192 <113277691+Miguelm192@users.noreply.github.com>
2024-11-21 03:44:54 -0500

    Update Qu2.cpp

--- Qu2.cpp
+++ Qu2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    vector<int> numbers;
+    vector<int> numbers; // Vector to store integers entered by the user
     int num;
 
     // Input numbers until -1 is entered
@@ -13,10 +13,10 @@ int main() {
         numbers.push_back(num); // Add the number to the vector
     }
 
-    // Check if there are enough numbers
-    if (numbers.size() < 2) {
+    // Check if there are enough numbers to find the second largest
+    if (numbers.size() < 2) { // If less than two numbers, display an error
         cout << "Not enough numbers to find the second largest!" << endl;
-        return 0;
+        return 0; // Exit the program
     }
 
     // Sort the vector in descending order
@@ -24,14 +24,15 @@ int main() {
 
     // Output the sorted vector
     cout << "Sorted vector (descending): ";
-    for (int n : numbers) {
-        cout << n << " ";
+    for (int n : numbers) { // Loop through the sorted vector
+        cout << n << " ";  // Print each number
     }
     cout << endl;
 
     // Output the largest and second largest numbers
-    cout << "Largest number: " << numbers[0] << endl;
-    cout << "Second largest number: " << numbers[1] << endl;
+    cout << "Largest number: " << numbers[0] << endl; // First element is the largest
+    cout << "Second largest number: " << numbers[1] << endl; // Second element is the second largest
 
-    return 0;
+    return 0; // End the program
+}
 }
